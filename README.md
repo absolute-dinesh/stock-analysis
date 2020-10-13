9dd0d054445d2a2ba82026acebbf3cffb1b7a4cb
Dinesh Reddy <maluchurudinesh@gmail.com>
2020-10-14 02:07:26 +0530

    Testing some features for nse

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # stock-analysis
 
 ## Analysis of NSE Stock market and tracking highvolume stocks traded in NSE
+
+https://pypi.org/project/nse/ 
+
+Mainly used this as this has good number of features
+
+https://pypi.org/project/nsetools/
+
+This is also worth noting.
--- src/nse/connect.py
+++ src/nse/connect.py
@@ -1,7 +1,12 @@
 from nse import Nse
 
 db = Nse()
+
+data = db.live("HDFCBANK")   
+print(data)
+
 #Load OIL Brand Data.
-data = db.load('OIL')
+data = db.load('HDFCBANK')
 #Bar Grap Of Data
-db.bar(data)
+Hdata = db.history("HDFCBANK","05-03-2020",'07-03-2020')
+print(Hdata)
