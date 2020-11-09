4847b25d71e50d63998db0e1e3570166a7ef1372
Dinesh Reddy <maluchurudinesh@gmail.com>
2020-11-10 00:17:28 +0530

    NSEtools basic

--- src/nse/connect.py
+++ src/nse/connect.py
@@ -8,5 +8,5 @@ print(data)
 #Load OIL Brand Data.
 data = db.load('HDFCBANK')
 #Bar Grap Of Data
-Hdata = db.history("HDFCBANK","05-03-2020",'07-03-2020')
+Hdata = db.history("HDFCBANK","05-10-2020",'15-10-2020')
 print(Hdata)
